c42ccfe512edaf6beef2caa7b17948a01c1a66e9
Derek D. Miller <dreemkiller@gmail.com>
2015-10-15 20:31:48 -0500

    Added __unicode__ methods to most of the model classes

--- lyker_dashboard/models.py
+++ lyker_dashboard/models.py
@@ -5,21 +5,29 @@ from django.db import models
 class Customer(models.Model):
     customer_id = models.IntegerField(default=0)
     name = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.name
 
 class Video(models.Model):
     video_id = models.IntegerField(default=0)
     customer = models.ForeignKey(Customer)
     name = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.name
 
 
 class Object(models.Model):
     object_id = models.IntegerField(default=0)
     object_name = models.CharField(max_length=200)
     video = models.ForeignKey(Video)
+    def __unicode__(self):
+        return self.object_name
 
 class User(models.Model):
     user_id = models.IntegerField(default=0)
     name = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.name
 
 class UserAction(models.Model):
     type = models.IntegerField(default=0)
