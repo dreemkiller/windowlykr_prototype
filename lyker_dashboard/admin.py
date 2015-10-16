94afef549cb495855e3af1bb65e90f3d2e8bee1f
Derek D. Miller <dreemkiller@gmail.com>
2015-10-15 20:37:13 -0500

    Added model classes to the admin access

--- lyker_dashboard/admin.py
+++ lyker_dashboard/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Customer
+from .models import Video
+from .models import Object
+from .models import User
+from .models import UserAction
+
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Video)
+admin.site.register(Object)
+admin.site.register(User)
+admin.site.register(UserAction)
+
+
