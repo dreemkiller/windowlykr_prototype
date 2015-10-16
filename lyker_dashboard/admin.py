9c28fc77a1a561c160fc515858170de4a06f40ff
Derek D. Miller <dreemkiller@gmail.com>
2015-10-15 21:04:12 -0500

    Updated the admin page to make a bit more sense

--- lyker_dashboard/admin.py
+++ lyker_dashboard/admin.py
@@ -1,16 +1,27 @@
 from django.contrib import admin
 
-from .models import Customer
-from .models import Video
-from .models import Object
-from .models import User
-from .models import UserAction
+from .models import Customer, Video, Object, User, UserAction
 
 # Register your models here.
 admin.site.register(Customer)
-admin.site.register(Video)
-admin.site.register(Object)
 admin.site.register(User)
 admin.site.register(UserAction)
+
+class ObjectInline(admin.TabularInline):
+    model = Object
+    extra = 4
+    list_filter = ['video']
+
+class VideoAdmin(admin.ModelAdmin):
+    inlines = [ObjectInline]
+    list_filter = ['customer']
+    search_fields = ['name']
+admin.site.register(Video, VideoAdmin)
+
+class ObjectAdmin(admin.ModelAdmin):
+    list_filter = ['video']
+    search_fields = ['object_name']
+
+admin.site.register(Object, ObjectAdmin)
 
 
