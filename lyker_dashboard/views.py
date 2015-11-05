a4475cd4510a09901664f3ff29072747985a1c44
Derek D. Miller <dreemkiller@gmail.com>
2015-11-04 21:06:56 -0600

    Changed all of the views to use get_object_or_404 and render with templates

--- lyker_dashboard/views.py
+++ lyker_dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 #from django.template import RequestContext, loader
 
@@ -10,31 +10,19 @@ def index(request):
     context = {'user_list': user_list,}
     return render(request, 'lyker_dashboard/index.html', context)
 def user(request, user_id):
-    try:
-        this_user = User.objects.get(user_id = user_id)
-    except User.DoesNotExist:
-        raise Http404("User does not exist")
-    return HttpResponse("User name:" + this_user.name)
+    this_user = get_object_or_404(User, pk=user_id)
+    return render(request, 'lyker_dashboard/user.html', {'this_user':this_user})
 
 def object(request, object_id):
-    try:
-        this_object = Object.objects.get(object_id = object_id)
-    except Object.DoesNotExist:
-        raise Http404("Object does not exist");
-    return HttpResponse("Object name:" + this_object.object_name)
+    this_object = get_object_or_404(Object, pk=object_id)
+    return render(request, 'lyker_dashboard/object.html', {'this_object': this_object})
 
 def customer(request, customer_id):
-    try:
-        this_customer = Customer.objects.get(customer_id = customer_id)
-    except Customer.DoesNotExist:
-        raise Http404("Customer does not exist")
-    return HttpResponse("customer name:" + this_customer.name)
+    this_customer = get_object_or_404(Customer, pk=customer_id)
+    return render(request, 'lyker_dashboard/customer.html', {'this_customer':this_customer})
 
 def video(request, video_id):
-    try:
-        this_video = Video.objects.get(video_id = video_id)
-    except Video.DoesNotExist:
-        raise Http404("Video does not exist")
-    return HttpResponse("video name:" + this_video.name)
+    this_video = get_object_or_404(Video, pk=video_id)
+    return render(request, 'lyker_dashboard/video.html', {'this_video':this_video})
 
 # Create your views here.
